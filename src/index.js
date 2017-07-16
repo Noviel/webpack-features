4c31acf54dbe5e9b8ca35e9097935e1ee08da436
Aleksandr Snov <snov.contact@gmail.com>
2017-07-16 21:20:58 +0700

    Export build manager

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { createBabelLoader } from './babel-chain-loader';
 export { isServer, isClient } from './target';
 
 export { default as EntryManager } from './entry-manager';
+export { default as BuildManager } from './build-manager';
