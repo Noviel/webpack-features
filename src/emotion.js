77c08fadfad6d57220e82cf9c022d22e046d23ef
Aleksandr Snov <snov.contact@gmail.com>
2017-10-30 21:42:03 +0700

    Set default value for emotion's option extractStatic to false

--- src/emotion.js
+++ src/emotion.js
@@ -1,14 +1,15 @@
-export default ({ target, production }, { extractStatic } = {}, state) => {
+export default (
+  { target, production },
+  { extractStatic = false } = {},
+  state
+) => {
   const babelLoaders = state.loaders(({ loader }) => loader === 'babel-loader');
 
   for (const loader of babelLoaders) {
     loader.options.plugins.unshift([
       'emotion',
       {
-        extractStatic:
-          extractStatic !== undefined
-            ? extractStatic
-            : target.browsers && production,
+        extractStatic,
       },
     ]);
   }
