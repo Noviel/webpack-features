a3be777be76697b3729046bae27b71fbb45a7313
Aleksandr Snov <snov.contact@gmail.com>
2018-01-25 16:34:47 +0700

    Fix  emotion export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { default as initialize } from './config';
 
 export { default as presetReact } from './presets/react';
+export { deafult as emotion } from './plugins/emotion';
 
 export default initialize;
-
-import { deafult as emotion } from './plugins/emotion';
