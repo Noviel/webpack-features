875a086195e94235a80c9ef97d2da61c0d8594cf
Aleksandr Snov <snov.contact@gmail.com>
2018-01-26 21:56:45 +0700

    Update tests for options fixes

--- src/features/__tests__/entry.spec.js
+++ src/features/__tests__/entry.spec.js
@@ -6,24 +6,16 @@ describe('entry', () => {
   describe('should produce correct entries with default options', () => {
     const entryOpts = { entries: { index: 'index.js' } };
     it('for modern browsers in production mode', () => {
-      expect(
-        entry(envs.modernBrowsersProd, entryOpts)
-      ).toMatchSnapshot();
+      expect(entry(envs.modernBrowsersProd, entryOpts)).toMatchSnapshot();
     });
     it('for modern browsers in development mode', () => {
-      expect(
-        entry(envs.modernBrowsersDev, entryOpts)
-      ).toMatchSnapshot();
+      expect(entry(envs.modernBrowsersDev, entryOpts)).toMatchSnapshot();
     });
     it('for legacy browsers in production mode', () => {
-      expect(
-        entry(envs.legacyBrowsersProd, entryOpts)
-      ).toMatchSnapshot();
+      expect(entry(envs.legacyBrowsersProd, entryOpts)).toMatchSnapshot();
     });
     it('for legacy browsers in development mode', () => {
-      expect(
-        entry(envs.legacyBrowsersDev, entryOpts)
-      ).toMatchSnapshot();
+      expect(entry(envs.legacyBrowsersDev, entryOpts)).toMatchSnapshot();
     });
   });
 });
