10dde4afc7613b391659c3558e6bf73952fd9ce7
Alexander Snov <snov.contact@gmail.com>
2018-04-26 17:55:50 +0700

    Use more convenient modern browsers list

--- src/lib/targets.js
+++ src/lib/targets.js
@@ -3,18 +3,7 @@ import type { TargetValue, Target } from './types';
 
 export const targets = {
   browsers: {
-    modern: [
-      'last 2 Chrome versions',
-      'not Chrome < 60',
-      'last 2 Safari versions',
-      'not Safari < 10.1',
-      'last 2 iOS versions',
-      'not iOS < 10.3',
-      'last 2 Firefox versions',
-      'not Firefox < 54',
-      'last 2 Edge versions',
-      'not Edge < 15',
-    ],
+    modern: ['>0.25%', 'not ie 11', 'not op_mini all'],
     legacy: ['> 1%', 'last 2 versions', 'Firefox ESR'],
   },
   node: '8.4',
